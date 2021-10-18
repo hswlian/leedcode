409b683b332252aa482954bad5116ca7f81c82ac
huashuwen <huashuwen@didiglobal.com>
2021-10-19 00:34:51 +0800

    fix: 增加剑指 offer 18 题

--- 剑指/Offer 18. 删除链表的节点.js
+++ 剑指/Offer 18. 删除链表的节点.js
@@ -15,3 +15,28 @@
 // 输出: [4,5,9]
 // 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var deleteNode = function(head, val) {
+   //如果在链表头部删除其头部那么直接head.next
+   if (head.val === val) return head.next;
+   let pre = head;
+   let cur = head.next;
+   while (cur !== null && cur.val !== val) {
+       pre = cur;
+       cur = cur.next;
+   }
+   if (cur && cur.val === val) pre.next = cur.next;
+   return head;
+};
+console.log(`======= result:${JSON.stringify(deleteNode({val: 4, next: {val: 5, next: {val: 3, next: {val: 9, next: null}}}}, 1))}`)
